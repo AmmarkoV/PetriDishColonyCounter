7e97277d26f4ec82892fe51195e6ed5821c5d097
Ammar Qammaz <ammarkov@gmail.com>
2020-08-13 00:53:13 +0300

    now detecting petri dish..

--- src/webcam.cpp
+++ src/webcam.cpp
@@ -4,8 +4,9 @@
  *  @author Ammar Qammaz (AmmarkoV)
  */
 #include <stdio.h>
+#include <vector>
 
-
+using namespace std;
 using namespace cv;
 
 
@@ -110,12 +111,18 @@ void eliminateDark(cv::Mat &rgb,int threshold)
 
 int main(int argc, char *argv[])
 {
-    cv::Mat rgb = imread(argv[1],CV_LOAD_IMAGE_COLOR);
+    cv::Mat rgb = imread(argv[1],cv::IMREAD_COLOR);
+
+    if(rgb.empty()){
+        printf(" Error opening image\n");
+        printf(" Program Arguments: [image_name] \n");
+        return 1;
+    }
 
     imshow("Input",rgb);
     cv::moveWindow("Input",0,0);
 
-    boostContrast(rgb,1.99,-250);
+    boostContrast(rgb,1.99,-210);
     eliminateDark(rgb,25);
       
     int dilation_size=1;
@@ -128,8 +135,13 @@ int main(int argc, char *argv[])
      cv::Mat kernel = getStructuringElement( dilation_type,
                        Size( 2*dilation_size + 1, 2*dilation_size+1 ),
                        Point( dilation_size, dilation_size ) );
+                       
+    kernel.at<float>(0,0)= 0;     kernel.at<float>(0,1)= 0;     kernel.at<float>(0,2)= 0;                      
+    kernel.at<float>(1,0)= 0;     kernel.at<float>(1,1)= 0;     kernel.at<float>(1,2)= 0;                      
+    kernel.at<float>(2,0)= 0;     kernel.at<float>(2,1)= 0;     kernel.at<float>(2,2)= 0;                      
 
-    //cv::dilate(rgb,rgb, kernel, Point(-1, -1), 2, 1, 1); 
+
+    cv::dilate(rgb,rgb, kernel, Point(-1, -1), 2, 1, 1); 
     //cv::erode(rgb,rgb, kernel, Point(-1, -1), 2, 1, 1);
     std::vector<KeyPoint> keypoints;
 
@@ -146,19 +158,19 @@ int main(int argc, char *argv[])
 
     // Filter by Area.
     params.filterByArea = true;
-    params.minArea = 10;
-    params.maxArea = 35;
+    params.minArea = 5;
+    params.maxArea = 44;
     
     // Filter by Circularity
     params.filterByCircularity = true;
-    params.minCircularity = 0.7;
+    params.minCircularity = 0.6;
 
     // Filter by Convexity
     params.filterByConvexity = false;
     params.minConvexity = 0.87;
 
     // Filter by Inertia
-    params.filterByInertia = false;
+    params.filterByInertia = true;
     params.minInertiaRatio = 0.01;
 
     cv::Ptr<cv::SimpleBlobDetector> detector = cv::SimpleBlobDetector::create(params);  // create blob detector
@@ -171,15 +183,46 @@ int main(int argc, char *argv[])
 
     fprintf(stderr,"Keypoints detected = %lu \n",keypoints.size());
 
+    Mat gray;
+    cvtColor(rgb, gray, COLOR_BGR2GRAY);
+    vector<Vec3f> circles;
+    HoughCircles(gray, 
+                 circles, 
+                 HOUGH_GRADIENT, 
+                 1, // 	Inverse ratio of the accumulator resolution to the image resolution. For example, if dp=1 , the accumulator has the same resolution as the input image
+                 gray.rows/2,  // 	Minimum distance between the centers of the detected circles.
+                 100, //the higher threshold of the two passed to the Canny edge detector (the lower one is twice smaller)
+                 50,  // it is the accumulator threshold for the circle centers at the detection stage. The smaller it is, the more false circles may be detected
+                 
+                 200, 300 // change the last two parameters
+            // (min_radius & max_radius) to detect larger circles
+    );
+
+
+
     // Draw detected blobs as red circles.
     // DrawMatchesFlags::DRAW_RICH_KEYPOINTS flag ensures the size of the circle corresponds to the size of blob
     Mat im_with_keypoints;
     drawKeypoints(rgb,keypoints,im_with_keypoints, Scalar(0,0,255), DrawMatchesFlags::DRAW_RICH_KEYPOINTS ); //DRAW_RICH_KEYPOINTS
 
+    for( size_t i = 0; i < circles.size(); i++ )
+    {
+        Vec3i c = circles[i];
+        Point center = Point(c[0], c[1]);
+        // circle center
+        circle( im_with_keypoints, center, 1, Scalar(0,100,100), 3, LINE_AA);
+        // circle outline
+        int radius = c[2];
+        circle( im_with_keypoints, center, radius, Scalar(255,0,255), 3, LINE_AA);
+    }
+
+
     char str[512]= {0};
     snprintf(str,512,"Counted blobs %lu",keypoints.size());
     cv::Point jointPoint(10,50);
     cv::putText(im_with_keypoints, str, jointPoint, cv::FONT_HERSHEY_DUPLEX, 1.5, cv::Scalar::all(255), 0.2, 8 );
+
+
 
     imshow("Output",im_with_keypoints);
     cv::moveWindow("Output",1000,0);
